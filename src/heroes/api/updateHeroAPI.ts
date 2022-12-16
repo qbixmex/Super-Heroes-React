846e9374cd0cff5ec455fc0e647502bc066a381f
Daniel González Briseño <qbixmex@gmail.com>
2022-12-16 14:22:31 -0600

    2.1.2-updating-hero-image
    - Api call updated

--- src/heroes/api/updateHeroAPI.ts
+++ src/heroes/api/updateHeroAPI.ts
@@ -9,18 +9,28 @@ type HeroData = {
 const { VITE_API_URL } = getEnvironmentVariables();
 
 export async function updateHero(formData: Hero): Promise<HeroData | void> {
+  const dataObject = new FormData();
+
+  formData.heroName && dataObject.append('heroName', formData.heroName);
+  formData.realName && dataObject.append('realName', formData.realName);
+  formData.studio && dataObject.append('studio', formData.studio);
+  formData.gender && dataObject.append('gender', formData.gender);
+  formData.image && dataObject.append('image', formData.image!);
+  formData.nationality && dataObject.append('nationality', formData.nationality);
+  formData.powers && dataObject.append('powers', formData.powers);
+
   const response = await fetch(`${VITE_API_URL}/heroes/${formData._id}`, {
     method: 'PATCH',
     headers: {
-      'Content-Type': 'application/json',
       'x-token': localStorage.getItem('token') ?? '',
     },
-    body: JSON.stringify(formData),
+    body: dataObject,
   });
 
   if (!response.ok) {
     const error = await response.json();
-    throw new Error(error.errors[0].msg);
+    if (error.msg) throw new Error(error.msg);
+    if (error.errors) throw new Error(error.errors[0].msg);
   }
 
   const data = await response.json() as HeroData;
