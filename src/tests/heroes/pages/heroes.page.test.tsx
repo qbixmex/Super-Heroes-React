04e99b342ef050c47fc0fcbd4eac8f3d33612812
Daniel González Briseño <qbixmex@gmail.com>
2022-12-21 23:45:43 -0600

    2.6.13-tests-on-hero-page-updated

--- src/tests/heroes/pages/heroes.page.test.tsx
+++ src/tests/heroes/pages/heroes.page.test.tsx
@@ -1,15 +1,20 @@
+import { Provider } from 'react-redux';
 import { describe, test } from 'vitest';
 import { render } from '@testing-library/react';
 import { HeroesPage } from '../../../heroes/pages/heroes.page';
+import { store } from '../../../store';
 
 describe('Test on <HeroesPage />', () => {
-  let container: HTMLElement;
-
   beforeEach(() => {
-    container = render(<HeroesPage />).container;
+    vi.clearAllMocks();
   });
 
   test('Should show component', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <HeroesPage />
+      </Provider>,
+    );
     const h1 = container.querySelector('h1');
     expect(h1?.innerHTML).toBe('Heroes');
   });
